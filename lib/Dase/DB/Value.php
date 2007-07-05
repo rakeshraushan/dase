09ba2e4fe9074b030248da5ed35cd16399a43e46
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-07-05 20:58:10 +0000

    get_xml_dump

--- lib/Dase/DB/Value.php
+++ lib/Dase/DB/Value.php
@@ -2,4 +2,12 @@
 
 class Dase_DB_Value extends Dase_DB_Autogen_Value 
 {
+	public $attribute;
+
+	function getAttribute() {
+		$att = new Dase_DB_Attribute;
+		$att->load($this->attribute_id);
+		$this->attribute = $att;
+		return $att;
+	}
 }
