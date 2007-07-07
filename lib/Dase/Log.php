d205a21b99d7eca567f7880c29d4b88763634c81
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-07-07 04:32:03 +0000

    added formatted date to logs

--- lib/Dase/Log.php
+++ lib/Dase/Log.php
@@ -3,18 +3,26 @@
 class Dase_Log 
 {
 	public static function logRemoteCall($msg) {
+		$date = date(DATE_W3C);
+		$msg = "$date : $msg\n";
 		file_put_contents(DASE_PATH .'/log/remote.log',$msg,FILE_APPEND);
 	}
 
 	public static function write($msg) {
+		$date = date(DATE_W3C);
+		$msg = "$date : $msg\n";
 		file_put_contents(DASE_PATH .'/log/standard.log',$msg,FILE_APPEND);
 	}
 
 	public static function error($msg) {
+		$date = date(DATE_W3C);
+		$msg = "$date : $msg\n";
 		file_put_contents(DASE_PATH .'/log/error.log',$msg,FILE_APPEND);
 	}
 
 	public static function sql($msg) {
+		$date = date(DATE_W3C);
+		$msg = "$date : $msg\n";
 		file_put_contents(DASE_PATH .'/log/sql.log',$msg,FILE_APPEND);
 	}
 }
