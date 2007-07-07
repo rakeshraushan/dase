8a9704a60c18a11163b0c09581d218311e506e71
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-07-07 03:12:47 +0000

    tweaked api handler

--- trunk/lib/Dase/ApiHandler.php
+++ trunk/lib/Dase/ApiHandler.php
@@ -3,19 +3,16 @@
 class Dase_ApiHandler 
 {
 	public static function index() {
-		echo "howdy, api"; exit;
-	}
-
-	public static function xml() {
 		//single collection request
 		$params = func_get_args();
 		if (isset($params[0])) {
 			$tpl = new Dase_Xml_Template;
 			$coll = new Dase_DB_Collection;
 			$coll->ascii_id = $params[0];
-			$coll->find(1);
-			$tpl->setXml($coll->xmlDump());
-			$tpl->display();
+			if ($coll->find(1)) {
+				$tpl->setXml($coll->xmlDump());
+				$tpl->display();
+			}
 		}
 	}
 }
