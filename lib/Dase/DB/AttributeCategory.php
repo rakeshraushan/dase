206d51ceaa61e3153c6370205dc9b492bf3e4c7d
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-07-13 22:06:43 +0000

    got rid of Attribute Categories

--- lib/Dase/DB/AttributeCategory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Dase/DB/Autogen/AttributeCategory.php';
-
-class Dase_DB_AttributeCategory extends Dase_DB_Autogen_AttributeCategory 
-{
-
-}
--- lib/Dase/DB/Category.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Dase/DB/Autogen/Category.php';
-
-class Dase_DB_Category extends Dase_DB_Autogen_Category 
-{
-
-}
