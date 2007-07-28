cbcbf43516f69495819506d5b2c3560365c26e63
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-07-28 05:15:36 +0000

    got rid of identifying att and parent it in itype

--- trunk/lib/Dase/DB/Autogen/AttributeItemType.php
+++ trunk/lib/Dase/DB/Autogen/AttributeItemType.php
@@ -5,7 +5,7 @@ require_once 'Dase/DB/Object.php';
 class Dase_DB_Autogen_AttributeItemType extends Dase_DB_Object 
 {
 	function __construct($assoc = false) {
-		parent::__construct( 'attribute_item_type',  array('........pg.dropped.4........','attribute_id','cardinality','item_type_id'));
+		parent::__construct( 'attribute_item_type',  array('attribute_id','cardinality','item_type_id'));
 		if ($assoc) {
 			foreach ( $assoc as $key => $value) {
 				$this->$key = $value;
--- trunk/lib/Dase/DB/Autogen/ItemType.php
+++ trunk/lib/Dase/DB/Autogen/ItemType.php
@@ -5,7 +5,7 @@ require_once 'Dase/DB/Object.php';
 class Dase_DB_Autogen_ItemType extends Dase_DB_Object 
 {
 	function __construct($assoc = false) {
-		parent::__construct( 'item_type',  array('ascii_id','collection_id','description','ident_attribute_id','name','parent_item_type_id'));
+		parent::__construct( 'item_type',  array('ascii_id','collection_id','description','name'));
 		if ($assoc) {
 			foreach ( $assoc as $key => $value) {
 				$this->$key = $value;
