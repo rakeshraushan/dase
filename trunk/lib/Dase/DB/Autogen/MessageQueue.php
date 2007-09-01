5740117adddfa3321f8ed8d6d50e60e7ea4a9158
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-09-01 16:17:27 +0000

    sql schema update -- moving to unix timestamps

--- trunk/lib/Dase/DB/Autogen/MessageQueue.php
+++ trunk/lib/Dase/DB/Autogen/MessageQueue.php
@@ -5,7 +5,7 @@ require_once 'Dase/DB/Object.php';
 class Dase_DB_Autogen_MessageQueue extends Dase_DB_Object 
 {
 	function __construct($assoc = false) {
-		parent::__construct( 'message_queue',  array('dase_user_eid','is_active','text','timestamp'));
+		parent::__construct( 'message_queue',  array('created','dase_user_eid','is_active','text'));
 		if ($assoc) {
 			foreach ( $assoc as $key => $value) {
 				$this->$key = $value;
