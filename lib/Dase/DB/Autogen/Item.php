a7dcec7272f0bfe82082d298c02ffad9f6ca34a7
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-09-01 16:17:27 +0000

    sql schema update -- moving to unix timestamps

--- lib/Dase/DB/Autogen/Item.php
+++ lib/Dase/DB/Autogen/Item.php
@@ -5,7 +5,7 @@ require_once 'Dase/DB/Object.php';
 class Dase_DB_Autogen_Item extends Dase_DB_Object 
 {
 	function __construct($assoc = false) {
-		parent::__construct( 'item',  array('collection_id','item_type_id','serial_number','status_id','timestamp'));
+		parent::__construct( 'item',  array('collection_id','created','item_type_id','serial_number','status_id'));
 		if ($assoc) {
 			foreach ( $assoc as $key => $value) {
 				$this->$key = $value;
