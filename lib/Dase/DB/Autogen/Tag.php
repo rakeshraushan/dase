4b48d4d8dca2e375814c189c7808f9cf2353d037
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-09-02 21:02:54 +0000

    schema mods

--- lib/Dase/DB/Autogen/Tag.php
+++ lib/Dase/DB/Autogen/Tag.php
@@ -5,7 +5,7 @@ require_once 'Dase/DB/Object.php';
 class Dase_DB_Autogen_Tag extends Dase_DB_Object 
 {
 	function __construct($assoc = false) {
-		parent::__construct( 'tag',  array('admin_collection_id','ascii_id','background','dase_user_id','description','is_public','master_item_id','name','tag_type_id','timestamp'));
+		parent::__construct( 'tag',  array('admin_collection_id','ascii_id','background','created','dase_user_id','description','is_public','master_item_id','name','tag_type_id'));
 		if ($assoc) {
 			foreach ( $assoc as $key => $value) {
 				$this->$key = $value;
