c6829ed4baf187ff56e60c0d0495d600c4c93d87
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-09-05 04:54:15 +0000

    dbadmin module

--- trunk/inc/config.php
+++ trunk/inc/config.php
@@ -4,6 +4,7 @@ $conf['modules']['vrc'] = 'vrc_collection';
 $conf['modules']['efossils'] = 'efossils_collection';
 $conf['modules']['elucy'] = 'efossils_collection';
 $conf['modules']['search'] = 1;
+$conf['modules']['dbadmin'] = 1;
 $conf['modules']['eid_auth'] = 0;
 
 $conf['superuser'][] = 'pkeane';
