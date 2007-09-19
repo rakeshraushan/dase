c3e18ebc798ddcfc3446fc3536d1e636b80def7b
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-09-19 14:26:22 +0000

    added att_ascii_id to value::getAttributeName

--- lib/Dase/DB/Value.php
+++ lib/Dase/DB/Value.php
@@ -6,9 +6,11 @@ class Dase_DB_Value extends Dase_DB_Autogen_Value
 {
 	public $attribute_name;
 
+	//this might need renaming????????
 	function getAttributeName() {
 		$a = new Dase_DB_Attribute();
 		$a->load($this->attribute_id);	
 		$this->attribute_name = $a->attribute_name;
+		$this->attribute_ascii_id = $a->ascii_id;
 	}
 }
