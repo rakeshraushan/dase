388d5b17517bec2767ea8fb899bead1e481706fa
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2007-10-10 20:26:41 +0000

    js optimization

--- trunk/scripts/dase_jquery.js
+++ trunk/scripts/dase_jquery.js
@@ -1,11 +1,11 @@
 if (!Dase) { var Dase = {}; }
-$(function(){ 
+jQuery(function(){ 
 		Dase.getUserTags();
 		Dase.listMenu();
-//		Dase.multicheck("checkedCollection");
-//		Dase.getItemTallies();
-//		Dase.initBrowse();
-//		Dase.initDynamicSearchForm();
+		Dase.multicheck("checkedCollection");
+		Dase.getItemTallies();
+		Dase.initBrowse();
+		Dase.initDynamicSearchForm();
 		}); 
 
 Dase.listMenu = function() { 
@@ -16,15 +16,43 @@ Dase.listMenu = function() {
 }
 
 Dase.multicheck = function(c) { 
-	$("ul.multicheck").append("<a href='' class='chk'>check all</a><a href='' class='unchk'>uncheck all</a>").find("input:checkbox").click( function() { $(this).parent().toggleClass(c);}).filter("@checked=checked").parent().addClass(c);
-	$(".chk").hide().click(function() { 
-			$(this).hide().next().show().parent().find("input:checkbox").attr("checked",true).parent().addClass(c);
-			return false;
-			});
-	$(".unchk").click(function() { 
-			$(this).hide().prev().show().parent().find("input:checkbox").removeAttr("checked").parent().removeClass(c);
-			return false;
-			});
+	var coll_list = document.getElementById('collectionList');
+	var multi = document.createElement('a');
+	multi.setAttribute('href','');
+	multi.setAttribute('class','uncheck');
+	multi.appendChild(document.createTextNode('check/uncheck all'));
+	coll_list.appendChild(multi);
+	var boxes = coll_list.getElementsByTagName('input');
+
+	multi.onclick = function() {
+		for (var i=0; i<boxes.length; i++) {
+			box = boxes[i];
+			if ('uncheck' == this.className) {
+				box.removeAttribute('checked');
+				box.nextSibling.nextSibling.className = '';
+			} else {
+				box.setAttribute('checked',true);
+				box.nextSibling.nextSibling.className = c;
+			}
+		}	   
+		if ('uncheck' == this.className) {
+			this.className = 'check';
+		} else {
+			this.className = 'uncheck';
+		}
+		return false;
+	}
+
+	for (var i=0; i<boxes.length; i++) {
+		boxes[i].onclick = function() {
+			var link = this.nextSibling.nextSibling;
+			if (c == link.className) {
+				link.className = '';
+			} else {
+				link.className = c;
+			}
+		}
+	}	   
 }
 
 Dase.getItemTallies = function() {
@@ -38,46 +66,50 @@ Dase.getItemTallies = function() {
 }
 
 Dase.getUserTags = function() {
-		var eid = $("#userData").text();
-		if (eid) {
-		Dase.getJSON("json/" + eid + "/tags",function(json){
-				for (var type in json[eid]) {
-				for (var ascii in json[eid][type]) {
-				var all = $("#tagsSelect").html() + "<input type='checkbox' name='" + ascii + "'> " + json[eid][type][ascii] + "</input><br>\n";
-				$("#tagsSelect").html(all);
-				var html = $("#" + type).html() + "<li><a href='" + eid + "/tag/" + ascii + "'>" + json[eid][type][ascii] + "</a></li>\n";
-				$("#" + type).html(html);
-					}
-					}
+	var eid = $("#userData").text();
+	if (eid) {
+		$.getJSON("json/" + eid + "/tags",function(json){
+				var tags={};
+				tags['tagsSelect'] = document.getElementById('tagsSelect');
+				var jsonEid = json[eid];
+				for (var type in jsonEid) {
+				var jsonType = jsonEid[type];
+				for (var ascii in jsonType) {
+				var jsonAscii = jsonType[ascii];
+				tags['tagsSelect'].innerHTML = tags['tagsSelect'].innerHTML + "<input type='checkbox' name='" + ascii + "'> " + jsonAscii + "</input><br>\n";
+				tags[type] = tags[type] ? tags[type] : document.getElementById(type);	
+				if (tags[type]) {
+				tags[type].innerHTML = tags[type].innerHTML + "<li><a href='" + eid + "/tag/" + ascii + "'>" + jsonAscii + "</a></li>\n";
+				} } } 
 				});
-		}
+	}
 }
 
 Dase.initBrowse = function() {
 	if($("#browseColumns").size()) {
 		Dase.getAttributes();
 		/*
-		$("#catColumn").find("a").each(function() {
-				var category_id = $(this).attr("id").split('_').pop(); //creates closure for click event
-				$(this).click(function() {
-					$("#attColumn").removeClass();
-					$('#valColumn').html(" ").attr("class","empty");
-					if ('admin' == category_id) {
-					Dase.getAdminAttributes(coll);
-					} else {
-					Dase.getAttributes(category_id,coll,public_only);
-					}
-					return false;
-					});
-				});
-				*/
+		   $("#catColumn").find("a").each(function() {
+		   var category_id = $(this).attr("id").split('_').pop(); //creates closure for click event
+		   $(this).click(function() {
+		   $("#attColumn").removeClass();
+		   $('#valColumn').html(" ").attr("class","empty");
+		   if ('admin' == category_id) {
+		   Dase.getAdminAttributes(coll);
+		   } else {
+		   Dase.getAttributes(category_id,coll,public_only);
+		   }
+		   return false;
+		   });
+		   });
+		 */
 	}
 }
 
 Dase.getAttributes = function() {
 	/*
-	Dase.getHtml('attColumn');
-	*/
+	   Dase.getHtml('attColumn');
+	 */
 	$("#attColumn").html("<div class='loading'>Loading Attributes...</div>");
 	var url = $("#attColumn").attr("class");
 	$.get(url,function(data) {
@@ -95,7 +127,7 @@ Dase.getAttributes = function() {
 
 Dase.getAdminAttributes = function(coll) {
 	var params = {
-	   token: new Date().getTime(),
+token: new Date().getTime(),
 	   link_class: "att_link",
 	   coll: coll
 	};	   
@@ -120,29 +152,29 @@ Dase.bindGetValues = function(coll) {
 			var attribute_id = $(this).attr("class").split(" ").pop(); //creates closure for click event
 			$(this).click(function() {
 				var params = {
-					token: new Date().getTime(),
-					attribute_id: attribute_id,
-					coll: coll
-					};	   
-					$("#attColumn//a[class=spill]").attr("class","att_link");
-					$("#att_link_"+attribute_id).attr("class","spill");
-					$("#valColumn").html("<div class='loading'>Loading Values...</div>");
-					$.get("ajax/values_by_attribute",params,function(data) {
-						$("#valColumn").html(data).removeClass();
-						});
-					$('#autocomplete').html(" ");
-					window.scroll(0,0);
-					return false;
-					});
+token: new Date().getTime(),
+attribute_id: attribute_id,
+coll: coll
+};	   
+$("#attColumn//a[class=spill]").attr("class","att_link");
+$("#att_link_"+attribute_id).attr("class","spill");
+$("#valColumn").html("<div class='loading'>Loading Values...</div>");
+$.get("ajax/values_by_attribute",params,function(data) {
+	$("#valColumn").html(data).removeClass();
+	});
+$('#autocomplete').html(" ");
+window.scroll(0,0);
+return false;
+});
 			});
-	Dase.getAttributeTallies(coll);
+Dase.getAttributeTallies(coll);
 }
 
 Dase.getAttributeTallies = function(coll) {
 	if ($("#getTallies").size()) {
 		var params = {
-         token: new Date().getTime(),
-		 coll: coll
+token: new Date().getTime(),
+	   coll: coll
 		};	   
 		if ('adminAtts' == $('#getTallies').attr("class")) { 
 			params.admin = 1;
@@ -170,55 +202,61 @@ Dase.initDynamicSearchForm = function() {
 			});
 }
 
-Dase.createXMLHttpRequest = function() {
-	var xmlhttp;
-	if (window.XMLHttpRequest) {
-		xmlhttp = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	} else {
-		alert('Perhaps your browser does not support xmlhttprequests?');
-	}
-	return xmlhttp;
+Dase.gid = function(id) {
+	return document.getElementById(id);
 }
 
-Dase.getHtml = function(elem_id,my_func) {
-	var target = document.getElementById(elem_id);
-	if (target) {
-		target.innerHTML = '<div class="loading">Loading...</div>';
-		var url = target.className;
-	}
-	var xmlhttp = Dase.createXMLHttpRequest(); //had to put constructor here so key-up functions work
-	xmlhttp.open('GET', url, true);
-	xmlhttp.send(null);
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			var returnStr = xmlhttp.responseText;
-			document.getElementById(elem_id).innerHTML = returnStr;
-			if (my_func) {
-				my_func();
-			}
-		} else {
-			// wait for the call to complete
-		}
-	}
+/*
+   Dase.createXMLHttpRequest = function() {
+   var xmlhttp;
+   if (window.XMLHttpRequest) {
+   xmlhttp = new XMLHttpRequest();
+   } else if (window.ActiveXObject) {
+   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+   } else {
+   alert('Perhaps your browser does not support xmlhttprequests?');
+   }
+   return xmlhttp;
+   }
+
+   Dase.getHtml = function(elem_id,my_func) {
+   var target = document.getElementById(elem_id);
+   if (target) {
+   target.innerHTML = '<div class="loading">Loading...</div>';
+   var url = target.className;
+   }
+   var xmlhttp = Dase.createXMLHttpRequest(); //had to put constructor here so key-up functions work
+   xmlhttp.open('GET', url, true);
+   xmlhttp.send(null);
+   xmlhttp.onreadystatechange = function() {
+   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+   var returnStr = xmlhttp.responseText;
+   document.getElementById(elem_id).innerHTML = returnStr;
+   if (my_func) {
+   my_func();
+   }
+   } else {
+// wait for the call to complete
+}
+}
 }
 
 Dase.getJSON = function(url,my_func) {
-	var xmlhttp = Dase.createXMLHttpRequest(); //had to put constructor here so key-up functions work
-	xmlhttp.open('GET', url, true);
-	xmlhttp.send(null);
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			var json = eval('(' + xmlhttp.responseText + ')');
-			if (my_func) {
-				my_func(json);
-			} else {
-				return json;
+var xmlhttp = Dase.createXMLHttpRequest(); //had to put constructor here so key-up functions work
+xmlhttp.open('GET', url, true);
+xmlhttp.send(null);
+xmlhttp.onreadystatechange = function() {
+if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+var json = eval('(' + xmlhttp.responseText + ')');
+if (my_func) {
+my_func(json);
+} else {
+return json;
 
-			}
-		} else {
-			// wait for the call to complete
-		}
-	}
 }
+} else {
+// wait for the call to complete
+}
+}
+}
+ */
