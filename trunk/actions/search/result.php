a1f751c3a1ab7d59078e262b2c2f718b4332965e
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-01-01 21:41:38 +0000

    deleted xslt/auth

--- trunk/actions/search/result.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$t = new Dase_Xslt(XSLT_PATH.'search/result.xsl');
-$t->set('local-layout',XSLT_PATH.'search/result.xml');
-$t->set('src',APP_ROOT.'/atom/'. $request_url . '?' . $query_string);
-Dase::display($t->transform());
-
