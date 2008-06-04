5a181fd8fb34ca7afdf6f0dcdf1d90e3e7ddcdd5
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-06-04 22:25:38 +0000

    atom archive processor

--- bin/process_atom.php
+++ bin/process_atom.php
@@ -6,16 +6,19 @@ $atom = Dase_Atom_Feed::load('american_west.atom');
 
 
 $collection = new Dase_DBO_Collection;
-$collection->collection_name = $atom->getName();
 $collection->ascii_id = $atom->getAscii_id();
+if ($collection->findOne()) {
+	print "collection exists\n";
+	exit;
+}
+$collection->collection_name = $atom->getName();
 $collection->path_to_media_files = 'do not know yet';
 $collection->is_public = 1;
 $collection->created = date(DATE_ATOM);
 $collection->updated = date(DATE_ATOM);
-//$collection->insert();
-//print_r($collection);
+$collection->insert();
+print "working on collection $collection->name\n";
 
-/*
 foreach ($atom->getAttributes() as $ascii => $name) {
 	$att = new Dase_DBO_Attribute;
 	$att->collection_id = $collection->id;
@@ -23,7 +26,7 @@ foreach ($atom->getAttributes() as $ascii => $name) {
 	$att->attribute_name = $name;
 	if (!$attribute->findOne()) {
 		$att->updated = date(DATE_ATOM);
-		//$att->insert();
+		$att->insert();
 	}
 }
 
@@ -34,12 +37,9 @@ foreach ($atom->getAdminAttributes() as $ascii => $name) {
 	$att->attribute_name = $name;
 	if (!$attribute->findOne()) {
 		$att->updated = date(DATE_ATOM);
-		//$att->insert();
+		$att->insert();
 	}
 }
- */
-
-
 
 foreach ($atom->entries as $entry) {
 	$item = Dase_DBO_Item::create($collection->ascii_id,$entry->getSerialNumber());
@@ -47,6 +47,19 @@ foreach ($atom->entries as $entry) {
 		foreach ($set['values'] as $v) {
 			$item->setValue($ascii,$v);
 		}
+	}
+	foreach ($entry->getMedia() as $med) {
+		$media_file = new Dase_DBO_MediaFile;
+		$media_file->item_id = $item->id;
+		$media_file->filename = basename($med['href']);
+		$media_file->width = $med['width'];
+		$media_file->height = $med['height'];
+		$media_file->size = $med['label'];
+		$media_file->mime_type = $med['type'];
+		$media_file->p_serial_number = $item->serial_number;
+		$media_file->p_collection_ascii_id = $collection->ascii_id;
+		$media_file->updated = date(DATE_ATOM);
+		$media_file->insert();
 	}
 }
 
