6525a1c63bc52bcb31bcfbd9ba02b324a988b429
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-07-15 01:39:09 +0000

    moved openid stuff into module

--- trunk/modules/friesen/actions/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$c = new Dase_Xml_Collection($collection_ascii_id);
-$keywords = $c->getAttVals('keyword');
-asort($keywords);
-
-$tpl = Dase_Template::instance('friesen');
-$tpl->assign('keywords',$keywords);
-$tpl->display('view_search_items.tpl');
-
--- trunk/modules/friesen/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Location: index");
-ini_set('display_errors','on');
-//ini_set('log_errors','on');
-//ini_set('error_log',DASE_PATH . '/log/error.log');
-error_reporting(E_ALL);
-exit;
-
