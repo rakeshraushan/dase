656d60bc915e201ad7c2177c13863bd8d13788a8
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-08-16 21:03:21 +0000

    no cache on installer

--- trunk/modules/install/handler.php
+++ trunk/modules/install/handler.php
@@ -37,7 +37,7 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		//file_put_contents(DASE_PATH.'/local_config.php',$file_contents);
 		$tpl = new Dase_Template($request,true);
 		$tpl->assign('conf',Dase_Config::getAll());
-		$request->renderResponse($tpl->fetch('index.tpl'));
+		$request->renderResponse($tpl->fetch('index.tpl'),false);
 	}
 
 	public function done($request) 
@@ -63,16 +63,16 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		try {
 			$db = new PDO($dsn, $user, $pass, $driverOpts);
 		} catch (PDOException $e) {
-			$request->renderResponse('no|connect failed: ' . $e->getMessage());
+			$request->renderResponse('no|connect failed: ' . $e->getMessage(),false);
 		}
 		try {
 			if (count(Dase_DB::listTables())) {
-				$request->renderResponse("ok|Database connection was successful ($count tables exist)");
+				$request->renderResponse("ok|Database connection was successful ($count tables exist)",false);
 			}
 		} catch (PDOException $e) {
 			//if we are here, passed in settings worked, but Dase_Config settings (saved settings) did not
 		}
 		//since connection, not table count, is dependent on passed in settings
-		$request->renderResponse("ready|Database connection was successful.");
+		$request->renderResponse("ready|Database connection was successful.",false);
 	}
 }
