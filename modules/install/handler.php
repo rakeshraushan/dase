2ce06b88d247583f5d03dce3ccde8a9422018a72
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-08-17 05:48:37 +0000

    working on installer

--- modules/install/handler.php
+++ modules/install/handler.php
@@ -8,6 +8,8 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		'index' => 'info',
 		'index/{msg}' => 'info',
 		'dbchecker' => 'dbchecker',
+		'dbinit' => 'dbinit',
+		'pathchecker' => 'pathchecker',
 	);
 
 	public function setup($request)
@@ -47,6 +49,8 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		//file_put_contents(DASE_PATH.'/local_config.php',$file_contents);
 		$tpl = new Dase_Template($request,true);
 		$tpl->assign('conf',Dase_Config::getAll());
+		$lc = DASE_PATH.'/inc/local_config.php';
+		$tpl->assign('lc',$lc);
 		$request->renderResponse($tpl->fetch('index.tpl'),false);
 	}
 
@@ -56,22 +60,38 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		$request->renderRedirect();
 	}
 
+	public function postToPathchecker($request) 
+	{
+		$resp = '';
+		if (is_writeable($request->get('path_to_media'))) {
+			$resp = "msg_ready|This path is writeable|";
+		} else {
+			$resp = "msg_no|This path is NOT writeable|";
+		}
+		if (is_writeable($request->get('graveyard'))) {
+			$resp .= "msg_ready|This path is writeable";
+		} else {
+			$resp .= "msg_no|This path is NOT writeable";
+		}
+		$request->renderResponse($resp,false);
+	}
+
 	public function postToDbchecker($request) 
 	{
-		$name = $request->get('db_name');
-		$path = $request->get('db_path');
-		$type = $request->get('db_type');
-		$host = $request->get('db_host');
-		$user = $request->get('db_user');
-		$pass = $request->get('db_pass');
-		$driverOpts = array();
-		if ('sqlite' == $type) {
-			$dsn = "sqlite:".$path;
+		$db = array();
+		$db['name'] = $request->get('db_name');
+		$db['path'] = $request->get('db_path');
+		$db['type'] = $request->get('db_type');
+		$db['host'] = $request->get('db_host');
+		$db['user'] = $request->get('db_user');
+		$db['pass'] = $request->get('db_pass');
+		if ('sqlite' == $db['type']) {
+			$dsn = "sqlite:".$db['path'];
 		} else {
-			$dsn = $type . ":host=$host;dbname=".$name;
+			$dsn = $db['type'].':host='.$db['host'].';dbname='.$db['name'];
 		}
 		try {
-			$db = new PDO($dsn, $user, $pass, $driverOpts);
+			$pdo = new PDO($dsn, $db['user'], $db['pass']);
 		} catch (PDOException $e) {
 			$request->renderResponse('no|connect failed: ' . $e->getMessage(),false);
 		}
@@ -84,5 +104,53 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		}
 		//since connection, not table count, is dependent on passed in settings
 		$request->renderResponse("ready|Database connection was successful.",false);
+	}
+
+	public function postToDbInit($request) 
+	{
+		$db = array();
+		$db['name'] = $request->get('db_name');
+		$db['path'] = $request->get('db_path');
+		$db['type'] = $request->get('db_type');
+		$db['host'] = $request->get('db_host');
+		$db['user'] = $request->get('db_user');
+		$db['pass'] = $request->get('db_pass');
+		if ('sqlite' == $db['type']) {
+			$dsn = "sqlite:".$db['path'];
+		} else {
+			$dsn = $db['type'].':host='.$db['host'].';dbname='.$db['name'];
+		}
+		try {
+			$pdo = new PDO($dsn, $db['user'], $db['pass']);
+		} catch (PDOException $e) {
+			$request->renderResponse('no|connect failed: ' . $e->getMessage(),false);
+		}
+		try {
+			if (count(Dase_DB::listTables())) {
+				$request->renderResponse("ok|Database is already initialized ($count tables exist)",false);
+			}
+		} catch (PDOException $e) {
+			//if we are here, passed in settings worked, but Dase_Config settings (saved settings) did not
+		}
+		//since connection, not table count, is dependent on passed in settings
+		$tpl = new Dase_Template($request,true);
+		$tpl->assign('eid',$request->get('eid'));
+		$tpl->assign('password',$request->get('password'));
+		$tpl->assign('path_to_media',$request->get('path_to_media'));
+		$tpl->assign('graveyard',$request->get('graveyard'));
+		$tpl->assign('db',$db);
+		$tpl->assign('token',md5(time().'abc'));
+		$tpl->assign('ppd_token',md5(time().'def'));
+		$tpl->assign('db',$db);
+		$config = $tpl->fetch('local_config.tpl');
+		$lc = DASE_PATH.'/inc/local_config.php';
+		if (!is_writeable($lc)) {
+			$request->renderResponse(
+				'nowrite|Cannot write to '.$lc.'. 
+				Save the following text as '.$lc.
+				':|'.$config
+			);
+		}
+		exit;
 	}
 }
--- modules/install/scripts/install.js
+++ modules/install/scripts/install.js
@@ -1,7 +1,26 @@
 Dase.install = {};
 
-Dase.install.initCheckDb = function() {
+Dase.install.initCheckForm = function() {
 	var form = Dase.$('check_form');
+
+	Dase.$('repos_check_button').onclick = function() {
+		Dase.$('init_db').className = 'hide';
+		Dase.$('local_config').className = 'hide';
+		Dase.$('init_db_msg').innerHTML = '';
+		var content_headers = {
+			'Content-Type':'application/x-www-form-urlencoded'
+		}
+		Dase.ajax(Dase.base_href+'pathchecker','post',function(resp) { 
+			parts = resp.split('|');
+			Dase.$('path_to_media_msg').className = parts[0];
+			Dase.$('path_to_media_msg').innerHTML = parts[1];
+			Dase.$('graveyard_msg').className = parts[2];
+			Dase.$('graveyard_msg').innerHTML = parts[3];
+		},Dase.form.serialize(form),null,null,content_headers); 
+		return false;
+	};
+
+	//deal with database type and sqlite db path input
 	var type_select = form.db_type;
 	var db_path = Dase.$('db_path');
 	if ('sqlite' != type_select.options[type_select.options.selectedIndex].value) {
@@ -16,8 +35,10 @@ Dase.install.initCheckDb = function() {
 			db_path.className = '';
 		}
 	}
-	var dbc = Dase.$('db_check_button');
-	dbc.onclick = function() {
+	Dase.$('db_check_button').onclick = function() {
+		Dase.$('init_db').className = 'hide';
+		Dase.$('local_config').className = 'hide';
+		Dase.$('init_db_msg').innerHTML = '';
 		var content_headers = {
 			'Content-Type':'application/x-www-form-urlencoded'
 		}
@@ -30,6 +51,8 @@ Dase.install.initCheckDb = function() {
 			}
 			if ('no' == parts[0]) {
 				db_msg.className = 'msg_no';
+				Dase.$('init_db').className = 'hide';
+				Dase.$('local_config').className = 'hide';
 			}
 			if ('ready' == parts[0]) {
 				db_msg.className = 'msg_ready';
@@ -38,9 +61,41 @@ Dase.install.initCheckDb = function() {
 		},Dase.form.serialize(form),null,null,content_headers); 
 		return false;
 	};
+	Dase.$('db_init_button').onclick = function() {
+		if ('' == Dase.util.trim(form.eid.value)) {
+			alert('please enter username');
+			return false;
+		}
+		if ('' == Dase.util.trim(form.password.value)) {
+			alert('please enter password');
+			return false;
+		}
+		var content_headers = {
+			'Content-Type':'application/x-www-form-urlencoded'
+		}
+		Dase.ajax(Dase.base_href+'dbinit','post',function(resp) { 
+			parts = resp.split('|');
+			var db_msg = Dase.$('init_db_msg');
+			db_msg.innerHTML = parts[1];
+			if ('ok' == parts[0]) {
+				db_msg.className = 'msg_ok';
+			}
+			if ('no' == parts[0]) {
+				db_msg.className = 'msg_no';
+				Dase.$('local_config').className = 'hide';
+				Dase.$('db_msg').innerHTML = '';
+			}
+			if ('nowrite' == parts[0]) {
+				db_msg.className = 'msg_no';
+				Dase.removeClass(Dase.$('local_config'),'hide');
+				Dase.$('local_config_txt').innerHTML = parts[2];
+			}
+		},Dase.form.serialize(form),null,null,content_headers); 
+		return false;
+	};
 };
 
 Dase.pageInit = function() {
-	Dase.install.initCheckDb();
+	Dase.install.initCheckForm();
 }
 
