b89ef04833969bbea73eb6b99a9898bcf57032f1
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-09-08 17:01:45 +0000

    added filter to get att vals 2

--- lib/Dase/Handler/Attribute.php
+++ lib/Dase/Handler/Attribute.php
@@ -9,25 +9,25 @@ class Dase_Handler_Attribute extends Dase_Handler
 		'{collection_ascii_id}/{att_ascii_id}/values' => 'attribute_values',
 	);
 
-	public function setup($request)
+	public function setup($r)
 	{
-		if ($request->has('collection_ascii_id')) {
-			$this->collection = Dase_DBO_Collection::get($request->get('collection_ascii_id'));
+		if ($r->has('collection_ascii_id')) {
+			$this->collection = Dase_DBO_Collection::get($r->get('collection_ascii_id'));
 		}
-		if ($request->has('att_ascii_id')) {
-			$this->attribute = Dase_DBO_Attribute::get($request->get('collection_ascii_id'),$request->get('att_ascii_id'));
+		if ($r->has('att_ascii_id')) {
+			$this->attribute = Dase_DBO_Attribute::get($r->get('collection_ascii_id'),$r->get('att_ascii_id'));
 		}
 	}
 
-	public function getAttributeJson($request) 
+	public function getAttributeJson($r) 
 	{
-		$request->renderResponse($this->attribute->asJson());
+		$r->renderResponse($this->attribute->asJson());
 	}
 
 	/** implicit 1000 limit */
-	public function getAttributeValuesJson($request) 
+	public function getAttributeValuesJson($r) 
 	{
-		$attr = Dase_DBO_Attribute::get($request->get('collection_ascii_id'),$request->get('att_ascii_id'));
+		$attr = Dase_DBO_Attribute::get($r->get('collection_ascii_id'),$r->get('att_ascii_id'));
 		if (0 == $attr->collection_id) {
 			//since it is admin att we need to be able to limit to items in this coll
 			$values_array = $attr->getDisplayValues($this->collection->ascii_id);
@@ -36,21 +36,21 @@ class Dase_Handler_Attribute extends Dase_Handler
 		}
 		$result['att_name'] = $attr->attribute_name;
 		$result['att_ascii'] = $attr->ascii_id;
-		$result['coll'] = $request->get('collection_ascii_id');
+		$result['coll'] = $r->get('collection_ascii_id');
 		$result['values'] = $values_array;
-		$request->renderResponse(Dase_Json::get($result));
+		$r->renderResponse(Dase_Json::get($result));
 	}
 
-	public function getAttributeValuesAtom($request) 
+	public function getAttributeValuesAtom($r) 
 	{
-		$attr = Dase_DBO_Attribute::get($request->get('collection_ascii_id'),$request->get('att_ascii_id'));
+		$attr = Dase_DBO_Attribute::get($r->get('collection_ascii_id'),$r->get('att_ascii_id'));
 		$key = '';
 		$val = '';
 		if ($r->has('filter_key') && $r->has('filter_value')) {
 			$key = $r->get('filter_key');
 			$val = $r->get('filter_value');
 		}
-		$request->renderResponse($attr->valuesAsAtom($request->get('collection_ascii_id'),$key,$val));
+		$r->renderResponse($attr->valuesAsAtom($r->get('collection_ascii_id'),$key,$val));
 	}
 
 }
