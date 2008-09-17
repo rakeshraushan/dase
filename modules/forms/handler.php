a59d3eae5e1fc6f97ec88fa93e86ada241fa6b60
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-09-17 22:05:58 +0000

    forms module work

--- modules/forms/handler.php
+++ modules/forms/handler.php
@@ -9,11 +9,13 @@ class Dase_ModuleHandler_Forms extends Dase_Handler {
 
 	public function setup($r)
 	{
+		$this->user = $r->getUser();
 	}
 
 	public function getNewForm($r) 
 	{
 		$tpl = new Dase_Template($r,true);
+		$tpl->assign('user',Utlookup::getRecord($this->user->eid));
 		$r->renderResponse($tpl->fetch('index.tpl'));
 	}
 }
