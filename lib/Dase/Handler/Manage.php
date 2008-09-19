5730d48c8eb724a488f3cb18ebeffd4c19f93c92
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-09-19 20:56:48 +0000

    coll visibility

--- lib/Dase/Handler/Manage.php
+++ lib/Dase/Handler/Manage.php
@@ -51,6 +51,7 @@ class Dase_Handler_Manage extends Dase_Handler
 			$this->collection->is_public = 1;
 		}
 		$this->collection->description = trim($r->get('description'));
+		$this->collection->visibility = $r->get('visibility');
 		$this->collection->update();
 		$params['msg'] = "settings updated";
 		$this->user->expireDataCache();
