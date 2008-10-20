cd1f9a86eee2ff30778490d7d0c4f4fa73125c00
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-10-20 00:45:48 +0000

    fixed tools config a bit

--- tools/config.php
+++ tools/config.php
@@ -4,10 +4,6 @@ define("BIN_PATH", dirname(__FILE__));
 define("DASE_PATH",BIN_PATH . '/..');
 define('CACHE_DIR', DASE_PATH . '/cache/');
 define("DASE_CONFIG", DASE_PATH . '/inc/config.php');
-define('CART',1);
-define('USER_COLLECTION',2);
-define('SLIDESHOW',3);
-define('LOG_LEVEL',3);
 
 define('DASE_LOG', DASE_PATH . '/var/log/dase.log');
 ini_set('include_path',ini_get('include_path').':'. DASE_PATH .'/lib:'); 
