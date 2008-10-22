ba5335b49562047be8be96e721acb870bed2a3b3
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-10-22 16:55:27 +0000

    working on forms

--- trunk/modules/install/handler.php
+++ trunk/modules/install/handler.php
@@ -163,6 +163,7 @@ class Dase_ModuleHandler_Install extends Dase_Handler {
 		}
 		$resp = array();
 		$dbconf = Dase_Config::get('db');
+		print $dbconf; exit;
 		$type = $dbconf['type'];
 		//todo: i need an sqlite schema as well
 		$table_prefix = Dase_Config::get('table_prefix');
