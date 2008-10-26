3ee10d0baeeb0b7d5e1835cc01dd6edf1559c966
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-10-26 19:09:44 +0000

    added category and bridge tables

---
+++ lib/Dase/DBO/Category.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Dase/DBO/Autogen/Category.php';
+
+class Dase_DBO_Category extends Dase_DBO_Autogen_Category 
+{
+
+}
---
+++ lib/Dase/DBO/CollectionCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Dase/DBO/Autogen/CollectionCategory.php';
+
+class Dase_DBO_CollectionCategory extends Dase_DBO_Autogen_CollectionCategory 
+{
+
+}
