ee7b21e2a75674160b0ccfe1b70758da199ae8c1
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2008-10-26 19:09:44 +0000

    added category and bridge tables

---
+++ trunk/lib/Dase/DBO/ItemCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Dase/DBO/Autogen/ItemCategory.php';
+
+class Dase_DBO_ItemCategory extends Dase_DBO_Autogen_ItemCategory 
+{
+
+}
---
+++ trunk/lib/Dase/DBO/ItemLinkCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Dase/DBO/Autogen/ItemLinkCategory.php';
+
+class Dase_DBO_ItemLinkCategory extends Dase_DBO_Autogen_ItemLinkCategory 
+{
+
+}
