f87506c7ae9a24cec0eebcff8893c6be33cb2e65
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-01-16 19:32:56 +0000

    item type items json

--- lib/Dase/Handler/ItemType.php
+++ lib/Dase/Handler/ItemType.php
@@ -130,10 +130,12 @@ class Dase_Handler_ItemType extends Dase_Handler
 		$items = array();
 		foreach ($t->getItems(500) as $item) {
 			$item = clone $item;
-			$items[$item->getBaseUrl()]  = array(
+			$item_array  = array(
+				'url' => $item->getBaseUrl(),
 				'title' => $item->getTitle(),
 				'serial_number' => $item->serial_number,
 			);
+			$items[] = $item_array;
 		}
 		$r->renderResponse(Dase_Json::get($items));
 	}
