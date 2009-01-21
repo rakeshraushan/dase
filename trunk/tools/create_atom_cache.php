078f4407279ca3f42d9bdbb11dd1a3133ca68025
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-01-21 15:09:47 +0000

    item_as_atom

--- trunk/tools/create_atom_cache.php
+++ trunk/tools/create_atom_cache.php
@@ -6,12 +6,16 @@ $coll = 'itsprop';
 
 Dase_Timer::start();
 $c = Dase_DBO_Collection::get($coll);
-$feed = new Dase_Atom_Feed;
 foreach ($c->getItems() as $item) {
-	//$entry = new Dase_Atom_Entry_Item;
-//	$item->injectAtomEntryData($entry,$c);
-	$item->injectAtomEntryData($feed->addEntry(),$c);
+	$entry = new Dase_Atom_Entry_Item;
+	$item->injectAtomEntryData($entry,$c);
+	$atom = new Dase_DBO_ItemAsAtom;
+	//should find or create
+	$atom->item_id = $item->id;
+	$atom->item_type_ascii_id = $item->getItemType()->ascii_id;
+	$atom->relative_url = 'item/'.$c->ascii_id.'/'.$item->serial_number;
+	$atom->updated = date(DATE_ATOM);
+	$atom->xml = $entry->asXml($entry->root); //so we don't get xml declaration
+	$atom->insert();
 }
 
-print $feed->asXml();
-print Dase_Timer::getElapsed();
