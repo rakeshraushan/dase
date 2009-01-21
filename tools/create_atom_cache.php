c566fe8e81ede0941e1dd0b7d137db540aae13c0
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-01-21 06:43:00 +0000

    xml work

--- tools/create_atom_cache.php
+++ tools/create_atom_cache.php
@@ -6,10 +6,12 @@ $coll = 'itsprop';
 
 Dase_Timer::start();
 $c = Dase_DBO_Collection::get($coll);
+$feed = new Dase_Atom_Feed;
 foreach ($c->getItems() as $item) {
-	$entry = new Dase_Atom_Entry_Item;
-	$item->injectAtomEntryData($entry,$c);
-	$r = $entry->asXml();
+	//$entry = new Dase_Atom_Entry_Item;
+//	$item->injectAtomEntryData($entry,$c);
+	$item->injectAtomEntryData($feed->addEntry(),$c);
 }
 
+print $feed->asXml();
 print Dase_Timer::getElapsed();
