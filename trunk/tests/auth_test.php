3b7bfcf7970ba77cda7949745a0b79c1da75cdcd
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-03-10 17:20:22 +0000

    getting rid of local tests configs

--- trunk/tests/auth_test.php
+++ trunk/tests/auth_test.php
@@ -4,10 +4,34 @@ error_reporting(E_ALL);
 ini_set('include_path','.:'.dirname(__FILE__).'/../lib');
 require_once('simpletest/autorun.php');
 require_once('simpletest/web_tester.php');
+require_once('Dase/Http/Request.php');
+require_once('Dase/Util.php');
 
 class AuthenticationTest extends WebTestCase {
+
+	function setUp() {
+	}
+
+	function tearDown() {
+	}
+
+	function test404Header() {
+		$r = new Dase_Http_Request(dirname(__FILE__).'/..');
+		$parts = explode('/',$r->app_root);
+		array_pop($parts);
+		$app_root = join('/',$parts);
+	//	$this->dump($app_root);
+		$this->get($app_root.'/xxxxxxxxxxxxx');
+		$this->assertResponse(404);
+	}
+
 	function test401Header() {
-		$this->get('http://quickdraw.laits.utexas.edu/dase1/user/pkeane/sets.atom');
+		$r = new Dase_Http_Request(dirname(__FILE__).'/..');
+		$parts = explode('/',$r->app_root);
+		array_pop($parts);
+		$app_root = join('/',$parts);
+	//	$this->dump($app_root);
+		$this->get($app_root.'/user/pkeane/sets.atom');
 		$this->assertAuthentication('Basic');
 		$this->assertResponse(401);
 		$this->assertRealm('DASe');
--- trunk/tests/cache_test.php
+++ trunk/tests/cache_test.php
@@ -5,49 +5,49 @@ ini_set('include_path','.:'.dirname(__FILE__).'/../lib');
 require_once('simpletest/autorun.php');
 require_once('Dase/Cache.php');
 require_once('Dase/Cache/File.php');
+require_once('Dase/Config.php');
 
 
 class TestOfCache extends UnitTestCase {
 
 	function setUp() {
-		$cache_dir = dirname(__FILE__).'/files/cache';
-		$cache = Dase_Cache::get('file',$cache_dir);
+		$c = new Dase_Config(dirname(__FILE__).'/..');
+		$c->load('inc/config.php');
+		$c->load('inc/local_config.php');
+		$cache = Dase_Cache::get('file',$c->getCacheDir());
 		$cache->expunge();
-		rmdir($cache_dir);
 	}
 
 	function tearDown() {
-		$cache_dir = dirname(__FILE__).'/files/cache';
-		$cache = Dase_Cache::get('file',$cache_dir);
+		$c = new Dase_Config(dirname(__FILE__).'/..');
+		$c->load('inc/config.php');
+		$c->load('inc/local_config.php');
+		$cache = Dase_Cache::get('file',$c->getCacheDir());
 		$cache->expunge();
-		rmdir($cache_dir);
-	}
-
-
-	function testCacheDirIsCreated() {
-		$cache_dir = dirname(__FILE__).'/files/cache';
-		$this->assertFalse(file_exists($cache_dir));
-		$cache = Dase_Cache::get('file',$cache_dir);
-		$this->assertTrue(file_exists($cache_dir));
 	}
 
 	/* NOTE: timestamp weirdness makes this fail
 	 * from command line.  It passes on web
 	 */
 	function testDataIsCached() {
-		$cache_dir = dirname(__FILE__).'/files/cache';
-		$cache = Dase_Cache::get('file',$cache_dir);
+		$c = new Dase_Config(dirname(__FILE__).'/..');
+		$c->load('inc/config.php');
+		$c->load('inc/local_config.php');
+		$this->dump($c->getCacheDir());
+		$cache = Dase_Cache::get('file',$c->getCacheDir());
 		$cache->setData('my_cache_file','hello world');
 		$data = $cache->getData('my_cache_file');
-		//$this->dump($cache->getData('my_cache_file'));
+		$this->dump($cache->getData('my_cache_file'));
 		$this->assertTrue('hello world' == $cache->getData('my_cache_file'));
 	}
 
 	/* NOTE: this is purposely slow
 	 */
 	function testDataIsExpired() {
-		$cache_dir = dirname(__FILE__).'/files/cache';
-		$cache = Dase_Cache::get('file',$cache_dir);
+		$c = new Dase_Config(dirname(__FILE__).'/..');
+		$c->load('inc/config.php');
+		$c->load('inc/local_config.php');
+		$cache = Dase_Cache::get('file',$c->getCacheDir());
 		$cache->setData('my_cache_file','hello world');
 		$data = $cache->getData('my_cache_file');
 		$this->dump(date(DATE_ATOM));
