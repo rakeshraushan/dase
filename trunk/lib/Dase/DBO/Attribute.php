85b66b7f29b35e81f98e860089365aa3618b9d2b
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-07-30 18:03:25 +0000

    work

--- trunk/lib/Dase/DBO/Attribute.php
+++ trunk/lib/Dase/DBO/Attribute.php
@@ -59,7 +59,7 @@ class Dase_DBO_Attribute extends Dase_DBO_Autogen_Attribute
 			throw new Exception('no such collection');
 		}
 		$att->collection_id = $coll->id;
-		$att->ascii_id = $attribute_ascii_id;
+		$att->ascii_id = Dase_Util::dirify($attribute_ascii_id);
 		if (!$att->findOne()) {
 			$attribute_ascii_id = array_pop(explode('#',$attribute_ascii_id)); //will use hashed value at end of URL
 			$att->attribute_name = ucwords(str_replace('_',' ',strtolower($attribute_ascii_id)));
@@ -79,7 +79,7 @@ class Dase_DBO_Attribute extends Dase_DBO_Autogen_Attribute
 	{
 		$att = new Dase_DBO_Attribute($db);
 		$att->collection_id = 0;
-		$att->ascii_id = $attribute_ascii_id;
+		$att->ascii_id = Dase_Util::dirify($attribute_ascii_id);
 		if (!$att->findOne()) {
 			$att->attribute_name = ucwords(str_replace('_',' ',$attribute_ascii_id));
 			$att->sort_order = 0;
