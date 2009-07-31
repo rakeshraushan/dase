6252a9d5df82b5d0b49d75819008fe2be7c9fb38
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-07-31 05:06:15 +0000

    set log perm

--- lib/Dase/Handler/Admin.php
+++ lib/Dase/Handler/Admin.php
@@ -222,7 +222,7 @@ class Dase_Handler_Admin extends Dase_Handler
 
 	public function postToSetLogPermission($r)
 	{
-		if (chmod(LOG_FILE,0644)) {
+		if (chmod(LOG_FILE,0664)) {
 			$params['msg'] = 'set OK';
 		} else {
 			$params['msg'] = 'no go';
