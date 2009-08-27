0060477b92fec3d9e4251d479cf69257d653ee0d
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2009-08-27 20:48:10 +0000

    direct help q access

--- lib/Dase/Handler/Help.php
+++ lib/Dase/Handler/Help.php
@@ -4,6 +4,7 @@ class Dase_Handler_Help extends Dase_Handler
 {
 	public $resource_map = array(
 		'/' => 'help',
+		'{identifier}' => 'answer',
 	);
 
 	protected function setup($r)
@@ -14,6 +15,17 @@ class Dase_Handler_Help extends Dase_Handler
 		$t = new Dase_Template($r);
 		$t->assign('collection',Dase_Atom_Feed::retrieve($r->app_root.'/collection/dase_help.atom?limit=100'));
 		$r->renderResponse($t->fetch('help.tpl'));
+	}
+
+	public function getAnswer($r) {
+
+		$ident = $r->get('identifier');
+		$search_result = Dase_Atom_Feed::retrieve($r->app_root.'/search.atom?c=dase_help&q=@identifier:"'.$ident.'"&limit=100');
+		foreach ($search_result->entries as $e) {
+			$meta = $e->getMetadata('answer');
+		}
+		$r->renderResponse($meta['text']);
+		$r->renderResponse($search_result->asXml());
 	}
 }
 
