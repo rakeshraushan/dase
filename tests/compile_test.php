9b8868b835d022759484709d15c7ab4d7f0d1eaf
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2010-02-14 19:53:43 +0000

    figuring out cache test problem3

--- tests/compile_test.php
+++ tests/compile_test.php
@@ -7,7 +7,11 @@ class TestOfCompile extends UnitTestCase {
 	function testAllSourceFilesCompile() {
 		$iter = new RecursiveDirectoryIterator(BASE_PATH.'/lib/Dase');
 		foreach (new RecursiveIteratorIterator($iter) as $file) {
-			if (false === strpos($file->getPathname(),'.svn') && strpos($file->getPathname(),'.php')) {
+			if (
+				false === substr($file->getPathname(),'.swp') && 
+				false === strpos($file->getPathname(),'.svn') && 
+				strpos($file->getPathname(),'.php')
+			) {
 				include_once $file->getPathname();
 			}
 		}
