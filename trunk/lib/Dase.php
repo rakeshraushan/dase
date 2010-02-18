6b5da47e9a7db84cab590cb30285578b4e699976
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2010-02-18 20:08:06 +0000

    moved local_bootstrap stuff into Dase.php

--- trunk/lib/Dase.php
+++ trunk/lib/Dase.php
@@ -9,14 +9,7 @@ class Dase
 		//sets up db object, does NOT try to connect
 		$db = new Dase_DB($config);
 
-		if (
-			file_exists(BASE_PATH.'/inc/local_bootstrap.php') &&
-			//make sure db is set up
-			file_exists(BASE_PATH.'/inc/local_config.php')
-		) {
-			//allows db & config enabled bootstrap
-			include BASE_PATH.'/inc/local_bootstrap.php';
-		}
+		$GLOBALS['app_data'] = self::initGlobalData($db,$config);
 
 		$r = new Dase_Http_Request();
 		$r->checkHandler($config->getAppSettings('default_handler'));
@@ -29,6 +22,32 @@ class Dase
 
 		$handler = $r->getHandlerObject($db,$config);
 		$handler->dispatch($r);
+	}
+
+	public static function initGlobalData($db,$config)
+	{
+		$cache = Dase_Cache::get($config);
+
+		//refreshed once per hour -- expunge when necessary!
+		$serialized_app_data = $cache->getData('app_data',3600);
+
+		if (!$serialized_app_data) {
+			$c = new Dase_DBO_Collection($db);
+			$colls = array();
+			$acl = array();
+			foreach ($c->find() as $coll) {
+				$colls[$coll->ascii_id] = $coll->collection_name;
+				$acl[$coll->ascii_id] = $coll->visibility;
+				//compat
+				$acl[$coll->ascii_id.'_collection'] = $coll->visibility;
+			}
+			$app_data['collections'] = $colls;
+			$app_data['media_acl'] = $acl;
+			$cache->setData('app_data',serialize($app_data));
+		} else {
+			$app_data = unserialize($serialized_app_data);
+		}
+		return $app_data;
 	}
 }
 
