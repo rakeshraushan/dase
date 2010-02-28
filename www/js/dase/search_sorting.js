77a5cfeb4e7c869c52208da8f8888a2d91cf3bb6
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2010-02-28 05:24:45 +0000

    fixed js bug

--- www/js/dase/search_sorting.js
+++ www/js/dase/search_sorting.js
@@ -20,7 +20,7 @@ Dase.initSearchSorting = function() {
 		form.onsubmit = function() {
 			var att = this.att_ascii_id.options[this.att_ascii_id.selectedIndex].value;
 			var current = window.location.href;
-			var pattern = /&sort=[a-z_]*/g;
+			var pattern = /&sort=[a-z0-9_]*/g;
 			var new_loc = current.replace(pattern,'')+'&sort='+att;
 			window.location.href = new_loc;
 			return false;
