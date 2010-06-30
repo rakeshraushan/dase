85c4d44a7e2cbe4b0e60c680a1d19065420d70e7
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2010-06-30 17:46:32 +0000

    bootstrap debug

--- trunk/inc/bootstrap.php
+++ trunk/inc/bootstrap.php
@@ -12,13 +12,6 @@ $config = new Dase_Config(BASE_PATH);
 $config->load('inc/config.php');
 $config->load('inc/local_config.php');
 
-if ($config->getAppSettings('force_https')) {
-	if ('on' != $_SERVER['HTTPS']) {
-		$secure_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		header("Location:$secure_url");
-	}
-}
-
 //imagemagick
 define('CONVERT',$config->getAppSettings('convert'));
 
@@ -29,6 +22,17 @@ define('DEBUG_LOG',$config->getLogDir().'/debug.log');
 
 //log level
 define('LOG_LEVEL',$config->getAppSettings('log_level'));
+
+Dase_Log::info(DEBUG_LOG,'in bootstrap');
+if ($config->getAppSettings('force_https')) {
+	Dase_Log::info(DEBUG_LOG,'force https is on');
+	if ('on' != $_SERVER['HTTPS']) {
+		Dase_Log::info(DEBUG_LOG,'server https is NOT on');
+		$secure_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		Dase_Log::info(DEBUG_LOG,'301ing to '.$secure_url);
+		header("Location:$secure_url");
+	}
+}
 
 //media directory
 define('MEDIA_DIR',$config->getMediaDir());
