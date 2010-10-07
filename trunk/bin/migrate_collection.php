79a6e308e0bb80916db2bac508c9e97a7dbb9678
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2010-10-07 20:41:22 +0000

    framework helpers

--- trunk/bin/migrate_collection.php
+++ trunk/bin/migrate_collection.php
@@ -1,13 +1,14 @@
 <?php
 include 'config.php';
 
-$source = 'diia';
-$target = 'cie';
+$source = 'asian_studies';
+$target = 'pkeane';
 
 $c = Dase_DBO_Collection::get($db,$source);
 
 foreach ($c->getItems() as $item) {
-	$json_doc = $item->retrieveJsonDoc('http://dase.laits.utexas.edu');
+	$json_doc = $item->buildJson('http://dase.laits.utexas.edu');
+	//print_r(json_decode($json_doc));
 	$target_url = 'https://daseupload.laits.utexas.edu/collection/'.$target.'/ingester';
 	print $target_url."\n";
 	$res = Dase_Http::post($target_url,$json_doc,'pkeane','dupload','application/json');
