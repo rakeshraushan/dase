d361b164ec28df9041296525a735d5e00ec04e24
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2011-04-28 18:05:20 +0000

    moved framework to framework_setup

---
+++ trunk/framework_setup/inc/local_config.php
@@ -0,0 +1,22 @@
+<?php
+
+$conf['db']['type'] = 'mysql';
+$conf['db']['host'] = 'mysql.laits.utexas.edu';
+$conf['db']['name'] = 't';
+$conf['db']['user'] = 'user';
+$conf['db']['pass'] = 'pass';
+$conf['db']['table_prefix'] = '';
+
+$conf['app']['main_title'] = 'my framework app';
+$conf['app']['default_handler'] = 'home';
+$conf['request_handler']['login'] = 'uteid';
+$conf['app']['user_class'] = 'Dase_DBO_User';
+$conf['app']['log_level'] = 3;
+$conf['app']['init_global_data'] = false;
+
+$conf['auth']['superuser']['pkeane'] = 'ok';
+$conf['auth']['token'] = 'auth';
+$conf['auth']['ppd_token'] = "ppd";
+$conf['auth']['service_token'] = "service";
+$conf['auth']['serviceuser']['test'] = 'ok';
+
