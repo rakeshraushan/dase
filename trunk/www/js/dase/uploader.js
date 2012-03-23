a33cb1fa97e80a5cdccceb982b80ba60d63746f2
pjkeane <pjkeane@8e959677-ca31-0410-912e-1dc59de9f185>
2012-03-23 19:59:03 +0000

    callback for word clouds

--- trunk/www/js/dase/uploader.js
+++ trunk/www/js/dase/uploader.js
@@ -14,63 +14,53 @@ Dase.pageInitUser = function(eid) {
 		h.attach(Dase.$('recent'));
 	});
 
-	/*
-	 $("#dropbox, #multiple").html5Uploader({ 
-		 name: "uploaded_file", 
-		 postUrl: "manage/"+coll,
-		 onClientLoadStart: Dase.makeFileList,
-	 onServerProgress: function() {Dase.showTime()},
-	 onSuccess: Dase.refresh
- }); 
- */
+	//fileTemplate += "<div class=\"progressbar\"></div>";
+	var fileTemplate = "<div id=\"{{id}}\">";
+	fileTemplate += "<div class=\"filename\">{{filename}}</div>";
+	fileTemplate += "<div class=\"preview\"></div>";
+	fileTemplate += "</div>";
 
-//fileTemplate += "<div class=\"progressbar\"></div>";
-var fileTemplate = "<div id=\"{{id}}\">";
-fileTemplate += "<div class=\"filename\">{{filename}}</div>";
-fileTemplate += "<div class=\"preview\"></div>";
-fileTemplate += "</div>";
-
-function slugify(text) {
-	text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
-	text = text.replace(/-/gi, "_");
-	text = text.replace(/\s/gi, "-");
-	return text;
-}
-$("#multiple").html5Uploader({
-	name: "uploaded_file", 
-	postUrl: "manage/"+coll,
-	onClientLoadStart: function (e, file) {
-		var upload = $("#upload");
-		if (upload.is(":hidden")) {
-			upload.show();
-		}
-		upload.append(fileTemplate.replace(/{{id}}/g, slugify(file.name)).replace(/{{filename}}/g, file.name));
-	},
-	onSuccess: function (e, file) {
-		$("#" + slugify(file.name)).find(".preview").html("uploaded!");
-	},
-	onClientLoad: function (e, file) {
-		$("#" + slugify(file.name)).find(".preview").append("<img src=\""+Dase.base_href+"www/images/indicator.gif\" alt=\"\">");
-	},
-	onServerLoadStart: function (e, file) {
-		$("#" + slugify(file.name)).find(".progressbar").progressbar({
-			value: 0
-		});
-	},
-	onServerProgress: function (e, file) {
-		if (e.lengthComputable) {
-			var percentComplete = (e.loaded / e.total) * 100;
+	function slugify(text) {
+		text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
+		text = text.replace(/-/gi, "_");
+		text = text.replace(/\s/gi, "-");
+		return text;
+	}
+	$("#multiple").html5Uploader({
+		name: "uploaded_file", 
+		postUrl: "manage/"+coll,
+		onClientLoadStart: function (e, file) {
+			var upload = $("#upload");
+			if (upload.is(":hidden")) {
+				upload.show();
+			}
+			upload.append(fileTemplate.replace(/{{id}}/g, slugify(file.name)).replace(/{{filename}}/g, file.name));
+		},
+		onSuccess: function (e, file) {
+			$("#" + slugify(file.name)).find(".preview").html("uploaded!");
+		},
+		onClientLoad: function (e, file) {
+			$("#" + slugify(file.name)).find(".preview").append("<img src=\""+Dase.base_href+"www/images/indicator.gif\" alt=\"\">");
+		},
+		onServerLoadStart: function (e, file) {
 			$("#" + slugify(file.name)).find(".progressbar").progressbar({
-				value: percentComplete
+				value: 0
+			});
+		},
+		onServerProgress: function (e, file) {
+			if (e.lengthComputable) {
+				var percentComplete = (e.loaded / e.total) * 100;
+				$("#" + slugify(file.name)).find(".progressbar").progressbar({
+					value: percentComplete
+				});
+			}
+		},
+		onServerLoad: function (e, file) {
+			$("#" + slugify(file.name)).find(".progressbar").progressbar({
+				value: 100
 			});
 		}
-	},
-	onServerLoad: function (e, file) {
-		$("#" + slugify(file.name)).find(".progressbar").progressbar({
-			value: 100
-		});
-	}
-});
+	});
 };
 
 /*
